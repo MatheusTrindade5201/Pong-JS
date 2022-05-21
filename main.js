020428c09bd04bccdd2a11388610cec12756b335
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-05-21 18:23:21 -0300

    Criação da bolinha

--- main.js
+++ main.js
@@ -6,4 +6,19 @@ function pintaCanvas(){
     pintar.fillRect(0, 0, 600, 400);
 }
 
+var xBolinha = 300;
+var yBolinha = 200;
+var raio = 8;
+
 pintaCanvas();
+
+function desenhaBolinha(){
+
+    pintar.fillStyle = 'white';
+    pintar.arc(xBolinha, yBolinha, raio, 0, 2 * Math.PI);
+    pintar.fill()
+
+
+}
+
+desenhaBolinha();
